4f8204bab213a1068c33671ec53d696800a26e6d
David <david.schmitz.privat@gmail.com>
2020-01-10 10:37:49 +0100

    Add circle build

---
+++ build_image.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -o nounset
+set -o errexit
+
+CREATED="$(date -u +%Y-%m-%dT%T%z)"
+REVISION="$(CIRCLE_SHA1)"
+BUILD_URL="$(CIRCLE_BUILD_URL)"
+SOURCE="$(CIRCLE_REPOSITORY_URL)"
+
+docker build \
+  -t "koenighotze/wordcloud:${REVISION}" \
+  --build-arg NAME="koenighotze/wordcloud" \
+  --build-arg CREATED="$CREATED" \
+  --build-arg TITLE="Wordcloud-cli docker wrappet" \
+  --build-arg REVISION="$REVISION" \
+  --build-arg BUILD_URL="$BUILD_URL" \
+  --build-arg SOURCE="$SOURCE" \
+  .
